d786891098a1c6121158b62cb014a30c026e8143
putraadityapriyono37 <putraadityap1212@gmail.com>
2024-12-19 17:59:21 +0700

    pwt

--- dashboard/index.php
+++ dashboard/index.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="col-lg-12">
         <h1>Dashboard</h1>
-        <p>Selamat datang <mark> <?= $_SESSION['user']; ?> </mark> di website RST WijayaKusuma</p>
+        <p>Selamat datang <mark> <?= $_SESSION['user']; ?> </mark> di website RST WijayaKusuma Purwokerto</p>
         <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
     </div>
 </div>
